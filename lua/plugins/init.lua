d937d6d961624502828cbfc7387ca5e581c360a1
ben <langley.ben98@gmail.com>
2024-08-03 20:33:26 +0000

    feat: added plugins folder
    using it like the docs say. Plugin configs go in the plugins folder and are automatically loaded by lazy. NEXT STEPS: refactor folder structure(dont need to split in years anymore everything can be in the init.lua)(after folder to plugins)

---
+++ lua/plugins/init.lua
@@ -0,0 +1,30 @@
+return {
+  {
+	  'nvim-telescope/telescope.nvim', tag = '0.1.6',
+	  -- or                            , branch = '0.1.x',
+	  dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  'NLKNguyen/papercolor-theme',
+  {'nvim-treesitter/nvim-treesitter',build = ':TSUpdate'},
+  "nvim-lua/plenary.nvim", -- don't forget to add this one if you don't have it yet!
+  {
+	  "ThePrimeagen/harpoon",
+	  branch = "harpoon2",
+	  dependencies = { {"nvim-lua/plenary.nvim"} }
+  },
+  'mbbill/undotree',
+  'mhinz/vim-signify',
+  {
+	  'VonHeikemen/lsp-zero.nvim',
+	  branch = 'v3.x',
+	  dependencies = {
+		  {'williamboman/mason.nvim'},
+		  {'williamboman/mason-lspconfig.nvim'},
+
+		  {'neovim/nvim-lspconfig'},
+		  {'hrsh7th/nvim-cmp'},
+		  {'hrsh7th/cmp-nvim-lsp'},
+		  {'L3MON4D3/LuaSnip'},
+	    }
+        },
+}
