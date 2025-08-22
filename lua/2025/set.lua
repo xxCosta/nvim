4607509aae4295adf9ccc9fb74605d28cd38edaa
ben <pimpdaddyjr69@icloud.com>
2025-08-22 00:29:02 -0300

    feat: line wrapping

--- lua/2025/set.lua
+++ lua/2025/set.lua
@@ -6,6 +6,11 @@ vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
+
+vim.opt.wrap = true
+vim.opt.linebreak = true   
+vim.opt.textwidth = 0   
+
 vim.opt.smartindent = true
 
 vim.opt.wrap = false
