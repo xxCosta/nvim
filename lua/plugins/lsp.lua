fdadbb76f5e2e28c94c7d20c7a70c337bff4744f
xxCosta <langley.ben98@gmail.com>
2026-01-22 19:34:53 -0400

    my lsp works
    no hackiness, no todo, this thing straight is straight up functional. i took this on becuz i wanted autocomplete while developping for sierra chart. I never really understood all this lsp stuff i just copied and pasted the configs usually lol and it always somehow worked. now that im into more niche development i had to have a custom lsp. this is the correct way to configure lsp's from mason. just keep following this and also go to nvim-lspconfig to see how to configure different ls'. pretty stoked ngl

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,52 @@
+return {
+    {
+        "williamboman/mason.nvim",
+        config = true,
+    },
+
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "neovim/nvim-lspconfig",
+            "hrsh7th/cmp-nvim-lsp",
+        },
+
+        opts = {
+            ensure_installed = { "lua_ls", "clangd" },
+
+        },
+    },
+
+    {
+        "neovim/nvim-lspconfig",
+
+        config = function()
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
+
+            vim.lsp.config("lua_ls",{
+                settings = {
+                    Lua = {
+                        diagnostics = {
+                            globals = {
+                                "vim"
+                            }
+                        }
+                    }
+                }
+
+            })
+
+            vim.lsp.config("clangd", {
+                -- capabilities = capabilities,
+                cmd = {
+                    "clangd",
+                    "--header-insertion=never",
+                    "--query-driver=*",
+                    "--compile-commands-dir=.",
+                },
+                filetypes = { "c", "cpp" },
+            })
+        end,
+    },
+}
+
