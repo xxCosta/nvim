3d2e6aa1305a71c737e9daa271d1a7f4785c95d7
xxCosta <langley.ben98@gmail.com>
2026-02-21 15:43:27 -0400

    prettier

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,48 +1,49 @@
 return {
-    {
-        'AlexvZyl/nordic.nvim',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            require('nordic').setup({
-                transparent = {
-                    bg = true,
-                    float = true
-                }
-            })
-            vim.cmd([[colorscheme nordic]])
-        end,
-    },
-    {
-        'nvim-telescope/telescope.nvim', tag = '0.1.6',
-        -- or                            , branch = '0.1.x',
-        dependencies = { {'nvim-lua/plenary.nvim'} }
-    },
-    'NLKNguyen/papercolor-theme',
-    "nvim-lua/plenary.nvim", -- don't forget to add this one if you don't have it yet!
-    {
-        "ThePrimeagen/harpoon",
-        branch = "harpoon2",
-        dependencies = { {"nvim-lua/plenary.nvim"} }
-    },
-    'mbbill/undotree',
-    'mhinz/vim-signify',
-    -- {
-    --     "mason-org/mason.nvim",
-    --     opts = {}
-    -- },
---    {
---      'VonHeikemen/lsp-zero.nvim',
---      branch = 'v3.x',
---      dependencies = {
---          {'williamboman/mason.nvim'},
---          {'williamboman/mason-lspconfig.nvim'},
---
---          {'neovim/nvim-lspconfig'},
---          {'hrsh7th/nvim-cmp'},
---          {'hrsh7th/cmp-nvim-lsp'},
---
---          { 'saadparwaiz1/cmp_luasnip' }
---      }
---  },
-  }
+  {
+    'AlexvZyl/nordic.nvim',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      require('nordic').setup({
+        transparent = {
+          bg = true,
+          float = true
+        }
+      })
+      vim.cmd([[colorscheme nordic]])
+    end,
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.6',
+    -- or                            , branch = '0.1.x',
+    dependencies = { {'nvim-lua/plenary.nvim'} }
+  },
+  'NLKNguyen/papercolor-theme',
+  "nvim-lua/plenary.nvim", -- don't forget to add this one if you don't have it yet!
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { {"nvim-lua/plenary.nvim"} }
+  },
+  'mbbill/undotree',
+  'mhinz/vim-signify',
+
+  -- {
+  --     "mason-org/mason.nvim",
+  --     opts = {}
+  -- },
+  --    {
+  --      'VonHeikemen/lsp-zero.nvim',
+  --      branch = 'v3.x',
+  --      dependencies = {
+  --          {'williamboman/mason.nvim'},
+  --          {'williamboman/mason-lspconfig.nvim'},
+  --
+  --          {'neovim/nvim-lspconfig'},
+  --          {'hrsh7th/nvim-cmp'},
+  --          {'hrsh7th/cmp-nvim-lsp'},
+  --
+  --          { 'saadparwaiz1/cmp_luasnip' }
+  --      }
+  --  },
+}
