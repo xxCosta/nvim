2b4bb65368a43c7dc290006dd70b149d8da936ff
xxCosta <langley.ben98@gmail.com>
2026-02-21 12:36:23 -0400

    common keymaps

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -23,9 +23,19 @@ return {
     config = function()
       local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
+      local on_attach = function(_, bufnr)
+        local opts = { buffer = bufnr }
 
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, opts)
+      end
 
       vim.lsp.config("lua_ls",{
+        on_attach = on_attach,
+        capabilities = capabilities,
         settings = {
           Lua = {
             diagnostics = {
@@ -39,7 +49,8 @@ return {
       })
 
       vim.lsp.config("clangd", {
-        -- capabilities = capabilities,
+        on_attach = on_attach,
+        capabilities = capabilities,
         cmd = {
           "clangd",
           "--header-insertion=never",
