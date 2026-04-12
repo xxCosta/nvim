46418dc299881e22ed927325fb7d79a56d39effc
xxCosta <langley.ben98@gmail.com>
2026-04-11 21:33:55 -0300

    new colorscheme

--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,4 +1,13 @@
 return {
-        vim.api.nvim_set_hl(0, "Normal", { bg = "none"}),
-        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none"})
+  {
+    'ThorstenRhau/token',
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme('token')
+      vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+      vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+      vim.api.nvim_set_hl(0, "CursorLineNr", { bg = "none" })
+    end,
+  },
 }
