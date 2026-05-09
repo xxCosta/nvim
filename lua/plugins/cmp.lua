727ea2003148c7aadad9eef5db14ea6add212649
xxCosta <langley.ben98@gmail.com>
2026-05-09 01:48:21 -0300

    autocomplete

---
+++ lua/plugins/cmp.lua
@@ -0,0 +1,32 @@
+
+return{
+  'saghen/blink.cmp',
+  dependencies = {
+    'saghen/blink.lib',
+    -- optional: provides snippets for the snippet source
+    'rafamadriz/friendly-snippets',
+  },
+  build = function()
+    -- build the fuzzy matcher, wait up to 60 seconds
+    -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
+    require('blink.cmp').build():wait(60000)
+  end,
+  opts = {
+    -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+    -- 'super-tab' for mappings similar to vscode (tab to accept)
+    -- 'enter' for enter to accept
+    -- 'none' for no mappings
+    --
+    -- All presets have the following mappings:
+    -- C-space: Open menu or open docs if already open
+    -- C-n/C-p or Up/Down: Select next/previous item
+    -- C-e: Hide menu
+    -- C-k: Toggle signature help (if signature.enabled = true)
+    --
+    -- See :h blink-cmp-config-keymap for defining your own keymap
+    keymap = { preset = 'default' },
+
+    -- (Default) Only show the documentation popup when manually triggered
+    completion = { documentation = { auto_show = true } },
+  },
+}
