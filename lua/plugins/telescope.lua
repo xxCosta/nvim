8b0795bf9277eb50b913a818bf9de1db273fba22
xxCosta <langley.ben98@gmail.com>
2026-05-24 16:33:58 -0300

    fast fzf to nvim config

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -5,14 +5,18 @@ return {
     version = '*',
     dependencies = {
       'nvim-lua/plenary.nvim',
-      { "nvim-tree/nvim-web-devicons",              opts = {} },
+      "nvim-tree/nvim-web-devicons",
       { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
     },
     config = function()
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = 'Telescope find files' })
       vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
-      vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+      vim.keymap.set('n', '<leader>fc', function()
+        builtin.find_files({
+          cwd = vim.fn.stdpath("config") .. "/lua/plugins"
+        })
+      end, { desc = 'config files' })
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
   }
