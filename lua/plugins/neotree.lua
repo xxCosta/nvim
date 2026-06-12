576d6ca54caae247ee474e66077039a79df9992c
xxCosta <langley.ben98@gmail.com>
2026-06-12 18:53:46 -0300

    Working Neotree
    pushed without testing first... it didnt work. this is the working
    version.

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -6,7 +6,23 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
+  lazy = false,
   config = function(_)
     vim.keymap.set("n", "<leader>rw", ":Neotree position=current<CR>")
-  end
+  end,
+  opts = {
+    filesystem = {
+      filtered_items = {
+        -- when true, they will just be displayed differently than normal items
+        visible = true,
+        -- whether children of filtered parents should inherit their parent's highlight group
+        children_inherit_highlights = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+        hide_ignored = false, -- hide files that are ignored by other gitignore-like files
+        -- other gitignore-like files, in descending order of precedence.
+        hide_hidden = false,  -- only works on Windows for hidden files/directories
+      },
+    }
+  },
 }
