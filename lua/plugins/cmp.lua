f146b09fd3019d06603c1d36e2c9693f75360ef1
saucedbenny <pimpdaddyjr69@icloud.com>
2026-08-29 18:44:43 -0300

    CMP Bug fixed
    i finally fixed a bug thats been a thorn in my side for a while...
    Remeber to rebuild the blink.lib ,not just blink, after you change
    parameters!!!!!!!

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -6,10 +6,13 @@ return {
     'rafamadriz/friendly-snippets',
   },
   build = function()
-    -- build the fuzzy matcher, wait up to 60 seconds
+    -- build the fuzzy matcher, optionally add a timeout to `pwait(timeout_ms)`
     -- you can use `gb` in `:Lazy` to rebuild the plugin as needed
     require('blink.cmp').build():pwait(10000)
   end,
+
+  ---@module 'blink.cmp'
+  ---@type blink.cmp.Config
   opts = {
     -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
     -- 'super-tab' for mappings similar to vscode (tab to accept)
@@ -26,6 +29,15 @@ return {
     keymap = { preset = 'default' },
 
     -- (Default) Only show the documentation popup when manually triggered
-    completion = { documentation = { auto_show = true } },
+    completion = { documentation = { auto_show = false } },
+
+    -- (Default) list of enabled providers defined so that you can extend it
+    -- elsewhere in your config, without redefining it, due to `opts_extend`
+    sources = { default = { 'lsp', 'path', 'snippets', 'buffer' } },
+
+    -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+    -- You may use a lua implementation instead by using `implementation = "lua"`
+    -- See the fuzzy documentation for more information
+    fuzzy = { implementation = "rust" }
   },
 }
