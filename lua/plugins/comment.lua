da39c8daaf02b22339be99b0a92e8617cca57507
saucedbenny <pimpdaddyjr69@icloud.com>
2026-09-24 15:39:01 -0300

    nil bug fix
    a temporary fix for comment.nvim until an official bug fix is released.
    shows nil when you try to comment on certain filtetypes

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -1,9 +1,12 @@
 --https://github.com/numToStr/Comment.nvim
 return {
-        {
-        'numToStr/Comment.nvim',
-            opts = {
-            -- add any options here
-            }
-        }
+  {
+    -- https://github.com/numToStr/Comment.nvim/issues/517#issuecomment-5388055135
+    -- I'm using this fix temporarily
+    'faergeek/Comment.nvim',
+    branch = "nvim-0.12-compatibility",
+    opts = {
+      -- add any options here
+    }
+  }
 }
